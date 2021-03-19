79dd4fdfaa2fbd900bd43611d4ac604d360f74e9
Ryan Brink <ryanbrink@protonmail.com>
2021-03-19 11:58:35 -0400

    typegod

--- lib/src/main/kotlin/org/leafygreens/torterra/ProviderSchema.kt
+++ lib/src/main/kotlin/org/leafygreens/torterra/ProviderSchema.kt
@@ -1,63 +1,187 @@
 package org.leafygreens.torterra
 
-import com.squareup.moshi.Json
-import com.squareup.moshi.JsonClass
+import com.squareup.moshi.*
 
 // Models -> https://www.terraform.io/docs/cli/commands/providers/schema.html#providers-schema-representation
 @JsonClass(generateAdapter = true)
 data class ProviderSchemaWrapper(
-    @Json(name = "format_version")
-    val formatVersion: String,
-    @Json(name = "provider_schemas")
-    val providerSchemas: Map<String, ProviderSchema>
+  @Json(name = "format_version")
+  val formatVersion: String,
+  @Json(name = "provider_schemas")
+  val providerSchemas: Map<String, ProviderSchema>
 )
 
 data class ProviderSchema(
-    val provider: SchemaRepresentation,
-    @Json(name = "resource_schemas")
-    val resourceSchemas: Map<String, SchemaRepresentation>,
-    @Json(name = "data_source_schemas")
-    val dataSourceSchemas: Map<String, SchemaRepresentation>
+  val provider: SchemaRepresentation,
+  @Json(name = "resource_schemas")
+  val resourceSchemas: Map<String, SchemaRepresentation>,
+  @Json(name = "data_source_schemas")
+  val dataSourceSchemas: Map<String, SchemaRepresentation>
 )
 
 data class SchemaRepresentation(
-    val version: Int,
-    val block: BlockRepresentation,
+  val version: Int,
+  val block: BlockRepresentation,
 )
 
 data class BlockRepresentation(
-    val attributes: Map<String, BlockAttributes>?,
-    @Json(name = "block_types")
-    val blockTypes: Map<String, BlockType>?,
-    @Json(name = "description_kind")
-    val descriptionKind: String?
+  val attributes: Map<String, BlockAttributes>?,
+  @Json(name = "block_types")
+  val blockTypes: Map<String, BlockType>?,
+  @Json(name = "description_kind")
+  val descriptionKind: String?
 )
 
+@JsonClass(generateAdapter = false)
 data class BlockAttributes(
-//    val type: String, // TODO oh boy dis gonna suck
-    val description: String?,
-    @Json(name = "description_kind")
-    val descriptionKind: String?,
-    val required: Boolean?,
-    val optional: Boolean?,
-    val computed: Boolean?,
-    val sensitive: Boolean?
+  val type: BlockAttributeType,
+  val description: String?,
+  @Json(name = "description_kind")
+  val descriptionKind: String?,
+  val required: Boolean?,
+  val optional: Boolean?,
+  val computed: Boolean?,
+  val sensitive: Boolean?
 )
+
+/* Simple
+ "type": "string"
+ */
+
+/* Collection
+ "type": [ "list", "string" ]
+ */
+
+/* Complex
+"type": [
+  "set",
+  [
+    "object",
+    {
+      "aux_address": [
+        "map",
+        "string"
+      ],
+      "gateway": "string",
+      "ip_range": "string",
+      "subnet": "string"
+    }
+  ]
+],
+ */
+
+enum class BlockAttributeCollection(val value: String) {
+  Set("set"),
+  List("list"),
+  Map("map"),
+  Obj("object");
+
+  companion object {
+    fun fromValue(str: String): BlockAttributeCollection = when (str) {
+      "set" -> Set
+      "list" -> List
+      "map" -> Map
+      "object" -> Obj
+      else -> error("Unexpected BlockAttributeCollection: $str")
+    }
+  }
+}
+
+sealed class BlockAttributeType() {
+  class SimpleBlockAttributeType(val type: String) : BlockAttributeType()
+  class NestedBlockAttributeType(val collection: BlockAttributeCollection, val nested: BlockAttributeType) : BlockAttributeType()
+  class ObjectBlockAttributeType(val types: Map<String, BlockAttributeType>) : BlockAttributeType()
+}
+
+class BlockAttributeTypeJsonAdapter : JsonAdapter<BlockAttributeType>() {
+  @FromJson
+  override fun fromJson(reader: JsonReader): BlockAttributeType? {
+    return when(reader.peek()) {
+      JsonReader.Token.BEGIN_ARRAY -> readTuple(reader)
+      JsonReader.Token.STRING -> BlockAttributeType.SimpleBlockAttributeType(reader.nextString())
+      else -> error("Unexpected peek token: ${reader.peek()}")
+    }
+  }
+
+  private fun readTuple(reader: JsonReader): BlockAttributeType {
+    reader.beginArray()
+    val first = BlockAttributeCollection.fromValue(reader.nextString())
+    val second = when(reader.peek()) {
+      JsonReader.Token.STRING -> BlockAttributeType.SimpleBlockAttributeType(reader.nextString())
+      JsonReader.Token.BEGIN_ARRAY -> readTuple(reader)
+      JsonReader.Token.BEGIN_OBJECT -> readObject(reader)
+      else -> error("Unexpected nested peek token: ${reader.peek()}")
+    }
+    reader.endArray()
+    return BlockAttributeType.NestedBlockAttributeType(first, second)
+  }
+
+  private fun readObject(reader: JsonReader): BlockAttributeType {
+    val map = mutableMapOf<String, BlockAttributeType>()
+    reader.beginObject()
+    while (reader.peek() == JsonReader.Token.NAME) {
+      val key = reader.nextName()
+      val value = when(reader.peek()) {
+        JsonReader.Token.BEGIN_ARRAY -> readTuple(reader)
+        JsonReader.Token.STRING -> BlockAttributeType.SimpleBlockAttributeType(reader.nextString())
+        else -> error("Unexpected peek token: ${reader.peek()}")
+      }
+      map[key] = value
+    }
+    reader.endObject()
+    return BlockAttributeType.ObjectBlockAttributeType(map)
+  }
+
+  @ToJson
+  override fun toJson(writer: JsonWriter, value: BlockAttributeType?) {
+    when(value) {
+      is BlockAttributeType.SimpleBlockAttributeType -> writer.value(value.type)
+      is BlockAttributeType.NestedBlockAttributeType -> writer.writeNested(value)
+      is BlockAttributeType.ObjectBlockAttributeType -> writer.writeObject(value)
+      null -> TODO()
+    }
+  }
+
+  private fun JsonWriter.writeNested(value: BlockAttributeType.NestedBlockAttributeType) {
+    beginArray()
+    value(value.collection.value)
+    when(value.nested) {
+      is BlockAttributeType.SimpleBlockAttributeType -> value(value.nested.type)
+      is BlockAttributeType.NestedBlockAttributeType -> writeNested(value.nested)
+      is BlockAttributeType.ObjectBlockAttributeType -> writeObject(value.nested)
+    }
+    endArray()
+  }
+
+  private fun JsonWriter.writeObject(value: BlockAttributeType.ObjectBlockAttributeType) {
+    beginObject()
+    value.types.forEach { (k, v) ->
+      name(k)
+      when(v) {
+        is BlockAttributeType.SimpleBlockAttributeType -> value(v.type)
+        is BlockAttributeType.NestedBlockAttributeType -> writeNested(v)
+        is BlockAttributeType.ObjectBlockAttributeType -> writeObject(v)
+      }
+    }
+    endObject()
+  }
+
+}
 
 enum class NestingMode {
-    single,
-    list,
-    set,
-    map
+  single,
+  list,
+  set,
+  map
 }
 
 data class BlockType(
-    @Json(name = "nesting_mode")
-    val nestingMode: NestingMode?,
-    val block: BlockRepresentation,
-    // todo only allowed on the list and set modes... how to represent programmatically?
-    @Json(name = "min_items")
-    val minItems: Int?,
-    @Json(name = "max_items")
-    val maxItems: Int?
+  @Json(name = "nesting_mode")
+  val nestingMode: NestingMode?,
+  val block: BlockRepresentation,
+  // todo only allowed on the list and set modes... how to represent programmatically?
+  @Json(name = "min_items")
+  val minItems: Int?,
+  @Json(name = "max_items")
+  val maxItems: Int?
 )
--- lib/src/main/kotlin/org/leafygreens/torterra/Torterra.kt
+++ lib/src/main/kotlin/org/leafygreens/torterra/Torterra.kt
@@ -1,6 +1,5 @@
 package org.leafygreens.torterra
 
-import com.squareup.moshi.Json
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
@@ -11,14 +10,14 @@ import kotlin.io.path.createTempDirectory
 
 object Torterra {
 
-    private const val NAME_MARKER = "{NAME}"
-    private const val SOURCE_MARKER = "{SOURCE}"
-    private const val VERSION_MARKER = "{VERSION}"
+  private const val NAME_MARKER = "{NAME}"
+  private const val SOURCE_MARKER = "{SOURCE}"
+  private const val VERSION_MARKER = "{VERSION}"
 
-    private const val TERRAFORM_INIT = "terraform init"
-    private const val TERRAFORM_PROVIDER_SCHEMA = "terraform providers schema -json"
+  private const val TERRAFORM_INIT = "terraform init"
+  private const val TERRAFORM_PROVIDER_SCHEMA = "terraform providers schema -json"
 
-    private val providerTemplate = """
+  private val providerTemplate = """
             terraform {
               required_providers {
                 $NAME_MARKER = {
@@ -29,48 +28,49 @@ object Torterra {
             }
         """.trimIndent()
 
-    @OptIn(ExperimentalPathApi::class)
-    fun generateJsonSchema(providerLookup: ProviderLookup): String {
-        val providerDir = createTempDirectory("torterra-${providerLookup.name}-").toFile()
-        val template = providerTemplate
-            .replace(NAME_MARKER, providerLookup.name)
-            .replace(SOURCE_MARKER, providerLookup.source)
-            .replace(VERSION_MARKER, providerLookup.version)
-        val mainTf = File("${providerDir.absolutePath}/main.tf")
-        mainTf.createNewFile()
-        mainTf.writeText(template)
-        TERRAFORM_INIT.runCommand(providerDir)
-        val schemaFile = File("${providerDir.absolutePath}/schema.json")
-        schemaFile.createNewFile()
-        TERRAFORM_PROVIDER_SCHEMA.runCommandToFile(providerDir, schemaFile)
-        return schemaFile.readText()
-    }
+  @OptIn(ExperimentalPathApi::class)
+  fun generateJsonSchema(providerLookup: ProviderLookup): String {
+    val providerDir = createTempDirectory("torterra-${providerLookup.name}-").toFile()
+    val template = providerTemplate
+      .replace(NAME_MARKER, providerLookup.name)
+      .replace(SOURCE_MARKER, providerLookup.source)
+      .replace(VERSION_MARKER, providerLookup.version)
+    val mainTf = File("${providerDir.absolutePath}/main.tf")
+    mainTf.createNewFile()
+    mainTf.writeText(template)
+    TERRAFORM_INIT.runCommand(providerDir)
+    val schemaFile = File("${providerDir.absolutePath}/schema.json")
+    schemaFile.createNewFile()
+    TERRAFORM_PROVIDER_SCHEMA.runCommandToFile(providerDir, schemaFile)
+    return schemaFile.readText()
+  }
 
-    fun convertToSchema(json: String): ProviderSchemaWrapper {
-        val moshi: Moshi = Moshi.Builder()
-            .addLast(KotlinJsonAdapterFactory())
-            .build()
-        val adapter: JsonAdapter<ProviderSchemaWrapper> = moshi.adapter(ProviderSchemaWrapper::class.java)
-        return adapter.fromJson(json) ?: error("Could not decode 😭")
-    }
+  fun convertToSchema(json: String): ProviderSchemaWrapper {
+    val moshi: Moshi = Moshi.Builder()
+      .add(BlockAttributeTypeJsonAdapter())
+      .addLast(KotlinJsonAdapterFactory())
+      .build()
+    val adapter: JsonAdapter<ProviderSchemaWrapper> = moshi.adapter(ProviderSchemaWrapper::class.java)
+    return adapter.fromJson(json) ?: error("Could not decode 😭")
+  }
 
-    private fun String.runCommand(workingDir: File) {
-        ProcessBuilder(*split(" ").toTypedArray())
-            .directory(workingDir)
-            .redirectOutput(ProcessBuilder.Redirect.INHERIT)
-            .redirectError(ProcessBuilder.Redirect.INHERIT)
-            .start()
-            .waitFor(60, TimeUnit.MINUTES)
-    }
+  private fun String.runCommand(workingDir: File) {
+    ProcessBuilder(*split(" ").toTypedArray())
+      .directory(workingDir)
+      .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+      .redirectError(ProcessBuilder.Redirect.INHERIT)
+      .start()
+      .waitFor(60, TimeUnit.MINUTES)
+  }
 
-    private fun String.runCommandToFile(workingDir: File, writeTo: File) {
-        ProcessBuilder(*split(" ").toTypedArray())
-            .directory(workingDir)
-            .redirectOutput(writeTo)
-            .redirectError(ProcessBuilder.Redirect.INHERIT)
-            .start()
-            .waitFor(60, TimeUnit.MINUTES)
-    }
+  private fun String.runCommandToFile(workingDir: File, writeTo: File) {
+    ProcessBuilder(*split(" ").toTypedArray())
+      .directory(workingDir)
+      .redirectOutput(writeTo)
+      .redirectError(ProcessBuilder.Redirect.INHERIT)
+      .start()
+      .waitFor(60, TimeUnit.MINUTES)
+  }
 
 }
 
@@ -79,14 +79,16 @@ fun main() {
 //    val testProvider = ProviderLookup("aws", "hashicorp/aws", "~> 3.0")
 //    val schema = Torterra.generateJsonSchema(testProvider)
 //    print(schema)
-    val snapshotPath = "lib/src/main/resources/"
+  val snapshotPath = "lib/src/main/resources/"
     val file = File("$snapshotPath/docker.json")
-    val json = file.readText()
-    val result = Torterra.convertToSchema(json)
-    println(result)
-    val moshi: Moshi = Moshi.Builder()
-        .addLast(KotlinJsonAdapterFactory())
-        .build()
-    val adapter: JsonAdapter<ProviderSchemaWrapper> = moshi.adapter(ProviderSchemaWrapper::class.java)
-    println(adapter.toJson(result))
+//  val file = File("$snapshotPath/aws.json")
+  val json = file.readText()
+  val result = Torterra.convertToSchema(json)
+  println(result)
+  val moshi: Moshi = Moshi.Builder()
+    .add(BlockAttributeTypeJsonAdapter())
+    .addLast(KotlinJsonAdapterFactory())
+    .build()
+  val adapter: JsonAdapter<ProviderSchemaWrapper> = moshi.adapter(ProviderSchemaWrapper::class.java)
+  println(adapter.toJson(result))
 }
